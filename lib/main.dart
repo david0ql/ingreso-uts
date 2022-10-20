c4a44d3dbba8a7aa6331ced6cc4dd56757bda7dd
David Gomez <davidgomez@Mac-mini-de-David.local>
2022-10-19 21:39:21 -0500

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ingreso UTS',
+      title: 'Ingreso UTS - David',
       initialRoute: 'splash',
       routes: {
         'home': (context) => const HomeScreen(),
